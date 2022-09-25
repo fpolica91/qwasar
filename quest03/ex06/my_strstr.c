2b16461297826660179db8d5849c6b8440f1816e
German policarpo <fabriciopolicarpo0@gmail.com>
2022-09-24 23:31:00 -0400

    R

--- quest03/ex06/my_strstr.c
+++ quest03/ex06/my_strstr.c
@@ -4,15 +4,18 @@
 
 char *my_strstr(char *haystack, char *needle)
 {
+  if (!needle || !*needle)
+    return haystack;
+
   int i;
 
-  if (!needle || !*needle)
-    return (haystack); // if needle is empty
   while (*haystack != '\0')
   {
     i = 0;
+    // iterate pointer until first char of needle matches nth char of haystack
     while (*haystack != *needle && *haystack != '\0')
       haystack++;
+    // simple character for character  comparison as long as chars in needle match chars in haystack
     while (haystack[i] == needle[i] && haystack[i] != '\0')
       i++;
     if (needle[i] == '\0')
@@ -22,5 +25,9 @@ char *my_strstr(char *haystack, char *needle)
   return NULL;
 }
 
-// issipi
-// missi ssippi \0
+int main()
+{
+  printf("%s:", my_strstr("hello", "ll"));
+}
+// function takes needle and finds it in haysack
+// if needle empty return haystack
