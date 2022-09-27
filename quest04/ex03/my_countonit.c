ac2df1edebd063fc11ae21e1dc0dfab3a632fe53
German policarpo <fabriciopolicarpo0@gmail.com>
2022-09-27 18:09:43 -0400

    using malloc

--- quest04/ex03/my_countonit.c
+++ quest04/ex03/my_countonit.c
@@ -21,17 +21,32 @@ typedef struct s_integer_array
 } integer_array;
 #endif
 
-integer_array *my_count_on_it(string_array *param_1)
+integer_array *my_count_on_it(string_array *p1)
 {
-    // declare pointer
-    integer_array *s_arr;
-    // allocate size according to size of array
 
-    // iterate over array
-    for (int i = 0; i < param_1->size; i++)
+    integer_array *pusher = (integer_array *)malloc(sizeof(integer_array));
+
+    pusher->size = p1->size;
+    pusher->array = (int *)malloc(sizeof(int) * p1->size);
+
+    for (int i = 0; i < p1->size; i++)
     {
-        s_arr->array[i] = strlen(param_1->array[i]);
+        pusher->array[i] = strlen(p1->array[i]);
     }
+    return pusher;
+}
 
-    return s_arr;
+int main()
+{
+    string_array *p1 = (string_array *)malloc(sizeof(string_array));
+    p1->size = 3;
+    p1->array = malloc(sizeof(char *) * 3);
+    p1->array[0] = "Hello";
+    p1->array[1] = "World";
+    p1->array[2] = "!";
+    integer_array *p2 = my_count_on_it(p1);
+    for (int i = 0; i < p2->size; i++)
+    {
+        printf("%d ", p2->array[i]);
+    }
 }
